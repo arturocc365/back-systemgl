82684e6d189d4e1283af5423df0123ddd3b2d75d
arturocc365 <arturocuadros365@gmail.com>
2022-01-20 01:31:04 -0500

    avance con la adecuacion

---
+++ src/main/java/com/systemgl/arturocuadros/BackSystemglApplication.java
@@ -0,0 +1,13 @@
+package com.systemgl.arturocuadros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackSystemglApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackSystemglApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/systemgl/arturocuadros/BackSystemglApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systemgl.arturocuadros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackSystemglApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
